358def15b076f771f9ec9abb7bcd511fe6ac7a01
bogdanvoicu <bogdan.voicu@luxon-tech.com>
2022-08-22 18:04:17 +0300

    module 4

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -1,10 +1,11 @@
 const gm = require('gm');
 const {parentPort, workerData} = require('worker_threads')
 
-gm(workerData.source).resize(100, 100).write(workerData.destination, (error) => {
-    if (error) {
-        throw  error;
+gm(workerData.source).resize(100, 100)
+    .write(workerData.destination, (error) => {
+        if (error) {
+            throw  error;
 
-    }
-    parentPort.postMessage({resized: true});
-});
+        }
+        parentPort.postMessage({resized: true});
+    });
--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const {Router, request} = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 
 const router = Router();
@@ -29,16 +30,20 @@ const upload = multer({
     storage: storage
 });
 
-const photoPath= path.resolve(__dirname,'../../client/photo-viewer.html');
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
-router.get('/photo-viewer',(request, response)=>{
+router.get('/photo-viewer', (request, response) => {
     response.sendFile(photoPath);
 })
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
     if (request.fileValidationError) {
         return response.status(400).json({error: request.fileValidationError});
     } else {
+        try {
+            await imageProcessor(request.file.filename);
+        } catch (e) {
+        }
         return response.status(201).json({success: true});
     }
 
